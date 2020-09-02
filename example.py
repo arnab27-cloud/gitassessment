848cf87f9b82aba689a7c9befa1bf428f1159998
arnab <arnabbiswas197@gmail.com>
2020-09-02 21:54:40 +0530

    added a simple greetings function

--- example.py
+++ example.py
@@ -1,4 +1,7 @@
 def git_operation():
  print("I am adding example.py file to the remote repository.")
+def greet(name):
+    print("Hello {}. Have a great day.".format(name))
 git_operation()
+greet(input())
 
